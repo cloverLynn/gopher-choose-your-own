3c1dc57a86549e2ff3fa0691b5e4a10a38e9f564
cForbes <clover.forbes96@gmail.com>
2024-04-09 01:29:36 -0400

    added some structs

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+type Chapter struct {
+	name    string
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
 // TODO READ JSON FILE
 func readJSON(s string) []any {
 
